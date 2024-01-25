a9338f6760d287066a90af23378a97b3bb627a29
adevancomp <adevancomp@gmail.com>
2024-01-25 13:06:57 -0400

    format Serial Print

--- firmware_sheila/firmware_sheila.ino
+++ firmware_sheila/firmware_sheila.ino
@@ -2,8 +2,13 @@
 
 /*
 author: Adevan Neves Santos
-dia: 18/01/2024
+day: 18/01/2024
+
+Data e Hora,Concentração de CO,Concentração de CO2,Umidade (%),Temperatura (°C)
+
 **/
+
+
 
 #include "CO2Sensor.h"
 #include "MQ7.h"
@@ -23,7 +28,7 @@ void setup() {
 
   Serial.println("");   // blank new line
 
-  Serial.println("Calibrating MQ7");
+  Serial.println("Calibrating MQ7 and co2 sensor");
   mq7.calibrate();    // calculates R0
   co2Sensor.calibrate();
   Serial.println("Calibration done!");
@@ -32,24 +37,27 @@ void setup() {
 void loop() {
   delay(1000);
   
-  int temperature = dht11.readTemperature();
+  double temperature = dht11.readTemperature();
   delay(50);
-  int humidity = dht11.readHumidity();
+  double humidity = dht11.readHumidity();
+  delay(10);
+  double co = mq7.readPpm();
+  delay(10);
+  double co2 = co2Sensor.read();
+  
   if (temperature != DHT11::ERROR_CHECKSUM && temperature != DHT11::ERROR_TIMEOUT &&
         humidity != DHT11::ERROR_CHECKSUM && humidity != DHT11::ERROR_TIMEOUT)
     {
-        Serial.print("Temperature: ");
-        Serial.print(temperature);
-        Serial.println(" °C");
-
-        Serial.print("Humidity: ");
+        /*Data e Hora,Concentração de CO,Concentração de CO2,Umidade (%),Temperatura (°C)*/
+        Serial.print("2023-12-01 00:00:00");
+        Serial.print(",");
+        Serial.print(co);
+        Serial.print(",");
+        Serial.print(co2);
+        Serial.print(",");
         Serial.print(humidity);
-        Serial.println(" %");
+        Serial.print(",");
+        Serial.print(temperature);
+        Serial.println(""); // linha em branco
     }
-  Serial.print("CO: PPM = "); 
-  Serial.print(mq7.readPpm());
-  Serial.print(" CO2: PPM = "); 
-  Serial.print(co2Sensor.read());
- 
-  Serial.println("");   // blank new line
 }
